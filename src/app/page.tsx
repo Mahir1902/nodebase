f7efa8ec2a489f88dc56412b1965c6a0073033b7
Mahir <mahirhaque7@gmail.com>
2026-04-03 00:43:53 +1100

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import { caller } from "@/trpc/server";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  const users = await caller.getUsers();
   return (
     <div className="">
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
       hello world
     </div>
   );
